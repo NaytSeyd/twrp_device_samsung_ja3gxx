fd673bd85cac224d0e2818c9e0ed8acf21169538
Fatih Ünsever <fatihunseverr@gmail.com>
2018-03-28 13:17:13 +0300

    bootimg.mk: update
    Signed-off-by: Fatih Ünsever <fatihunseverr@gmail.com>

--- bootimg.mk
+++ bootimg.mk
@@ -1,4 +1,5 @@
 LZMA_BIN := $(shell which lzma)
+ODIN_TAR_OUT ?= $(PRODUCT_OUT)/recovery.tar
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel)
 	@echo ----- Compressing recovery ramdisk with lzma ------
@@ -9,3 +10,5 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_k
 	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	@echo ----- Made recovery image -------- $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	$(hide) tar -C $(PRODUCT_OUT) -H ustar -c recovery.img > $(ODIN_TAR_OUT)
+	@echo ----- Made Odin flashable recovery tar: ${ODIN_TAR_OUT} -----
